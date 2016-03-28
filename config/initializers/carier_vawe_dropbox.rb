761e6e7312e5f690831271125ea181b5671e4c4d
begin29 <vadim.motsuch@gmail.com>
2016-03-28 15:54:55 +0300

    add dropbox storage for production

--- config/initializers/carier_vawe_dropbox.rb
+++ config/initializers/carier_vawe_dropbox.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = ENV["APP_KEY"]
-  config.dropbox_app_secret = ENV["APP_SECRET"]
-  config.dropbox_access_token = ENV["ACCESS_TOKEN"]
-  config.dropbox_user_id = ENV["USER_ID"]
+  config.dropbox_app_key = ENV["DROPBOX_APP_KEY"]
+  config.dropbox_app_secret = ENV["DROPBOX_APP_SECRET"]
+  config.dropbox_access_token = ENV["DROPBOX_ACCESS_TOKEN"]
+  config.dropbox_user_id = ENV["DROPBOX_USER_ID"]
   config.dropbox_access_type = "dropbox"
 end
